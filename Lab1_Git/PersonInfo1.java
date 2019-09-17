bb9d4ba9f4fa0e55fca84ef5982fd3775c883f0e
MySwampert <tribenj@gmail.com>
2019-09-17 18:09:47 -0400

    first commit

--- Lab1_Git/PersonInfo1.java
+++ Lab1_Git/PersonInfo1.java
@@ -1,9 +1,9 @@
 public class PersonInfo1 {
 
-    public static String firstName = "Leisha";
-    public static String lastName = "Baker";
-    public static int studentNo = 338299;
-    public static String description = "Likes to take pictures";
+    public static String firstName = "Tristan";
+    public static String lastName = "Benoit";
+    public static int studentNo = 300058143;
+    public static String description = "likes to play video games... what did you expect?";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
@@ -11,18 +11,18 @@ public class PersonInfo1 {
         out += SIConstants.STUDENT_NUMBER_FR + studentNo + '.';
         out += SIConstants.DESCRIPTION_FR + description + '.';
         out += SIConstants.EOL;
-        
+
         System.out.println(out);
     }
-    
-    
+
+
     public static void displayEn() {
         String out = SIConstants.GREETING_EN;
         out += " " + firstName + " " + lastName + '.';
         out += SIConstants.STUDENT_NUMBER_EN + studentNo + '.';
         out += SIConstants.DESCRIPTION_EN + description + '.';
         out += SIConstants.EOL;
-        
+
         System.out.println(out);
     }
 }
